43adabe17ae17b2726e92053d4a65be9c79100b6
Janik Schönbeck <59802740+333Jay333@users.noreply.github.com>
2026-05-27 18:00:35 +0200

    added modals for archiving abos

--- webapp/modules/mod_abos.R
+++ webapp/modules/mod_abos.R
@@ -1,11 +1,11 @@
+choices_abos <- setNames(
+  c(10,3,6), # values (what server receives)
+  c("10er-Abo", "3-Monats-Abo", "6-Monats-Abo") # labels (what user sees)
+)
+
 mod_abos_ui <- function(id) {
   ns <- NS(id)
-  
-  choices_abos <- setNames(
-    c(10,3,6), # values (what server receives)
-    c("10er-Abo", "3-Monats-Abo", "6-Monats-Abo") # labels (what user sees)
-  )
-  
+
   tagList(
     h3("Abos verwalten"),
     
@@ -48,51 +48,7 @@ mod_abos_ui <- function(id) {
 mod_abos_server <- function(id, con, global_refresh) {
   moduleServer(id, function(input, output, session) {
     
-    # update choices for input$course -> gets refreshed if new courses get added
-    observeEvent(global_refresh$courses, {
-      courses <- get_courses(con)
-      choices_courses <- setNames(
-        courses$course_id, # values (what server receives)
-        courses$kursname # labels (what user sees)
-      )
-      
-      updateSelectInput(
-        session,
-        "course",
-        choices = choices_courses
-      )
-    })
-    
-    # change value for price input based on selected inputs
-    observeEvent(input$abo, {
-      req(input$course)
-      
-      price <- get_abo_price(con, input$course, input$abo)$abo_price
-      
-      updateNumericInput(
-        session,
-        "new_price",
-        value = price
-      )
-    })
-    observeEvent(input$course, {
-      req(input$abo)
-      
-      price <- get_abo_price(con, input$course, input$abo)$abo_price
-      
-      updateNumericInput(
-        session,
-        "new_price",
-        value = price
-      )
-    })
-    
-    # UPDATE
-    observeEvent(input$update_price, {
-      req(input$course, input$abo, input$new_price)
-      
-      update_abo_price(con, input$course, input$abo, input$new_price)
-    })
+    ns <- session$ns  # THIS is your ns function in server -> needed for modals
     
     # Abgelaufene Abos
     
@@ -168,6 +124,151 @@ mod_abos_server <- function(id, con, global_refresh) {
         selection = "single",
         options = list(pageLength = 10)
       )
+    })
+    
+    
+    
+    # DELETE
+    
+    # reactive value to store if a abo 10 or abo month is being archived
+    rv_archive_type <- reactiveVal()
+    
+    ## Modals
+    show_archive_modal <- function() {
+      
+      showModal(
+        modalDialog(
+          title = "Abo archivieren",
+          
+          "Was soll nach der Abo-Archivierung passieren?",
+          
+          footer = tagList(
+            modalButton("Abbrechen"),
+            
+            actionButton(
+              ns("archive_only"),
+              "Nur archivieren"
+            ),
+            
+            actionButton(
+              ns("replace_abo"),
+              "Archivieren und ein neues Abo erstellen"
+            )
+          )
+        )
+      )
+    }
+    
+    show_add_abo_modal <- function() {
+      showModal(
+        modalDialog(
+          title = "Neues Abo erstellen",
+          
+          selectInput(ns("abo"), "Abo wählen", choices = choices_abos),
+          dateInput(ns("abo_start"), "Abo-Beginn", format = "dd.mm.yyyy"),
+          
+          footer = tagList(
+            modalButton("Abbrechen"),
+            actionButton(ns("confirm_new_abo"), "Abo erstellen")
+          )
+        )
+      )
+    }
+    
+    ## functions
+    get_selected_row <- function() {
+      if (rv_archive_type() == "10") {
+        selected <- input$abo_10_expired_rows_selected
+      } else {
+        selected <- input$abo_month_expired_rows_selected
+      }
+
+      # Safety check
+      if (length(selected) == 0) {
+        showNotification("Bitte Teilnehmer*in auswählen", type = "warning")
+        return()
+      }
+
+      # get selected row data
+      if (rv_archive_type() == "10") {
+        data <- data_abo_10_expired()[selected, ]
+      } else {
+        data <- data_abo_month_expired()[selected, ]
+      }
+      
+      # return
+      data
+    }
+    
+    ## server logic
+    # When user selects an abo to remove, the first modal is shown
+    observeEvent(input$archive_abo_10, {
+      show_archive_modal()
+      rv_archive_type("10")
+    })
+    observeEvent(input$archive_abo_month, {
+      show_archive_modal()
+      rv_archive_type("month")
+    })
+    
+    # in case abo only needs to be archived
+    observeEvent(input$archive_only, {
+      print(get_selected_row())
+    })
+    
+    observeEvent(input$replace_abo, {
+      removeModal()
+      
+      show_add_abo_modal()
+      
+    })
+    
+    # Abo price
+    
+    # update choices for input$course -> gets refreshed if new courses get added
+    observeEvent(global_refresh$courses, {
+      courses <- get_courses(con)
+      choices_courses <- setNames(
+        courses$course_id, # values (what server receives)
+        courses$kursname # labels (what user sees)
+      )
+      
+      updateSelectInput(
+        session,
+        "course",
+        choices = choices_courses
+      )
+    })
+    
+    # change value for price input based on selected inputs
+    observeEvent(input$abo, {
+      req(input$course)
+      
+      price <- get_abo_price(con, input$course, input$abo)$abo_price
+      
+      updateNumericInput(
+        session,
+        "new_price",
+        value = price
+      )
+    })
+    observeEvent(input$course, {
+      req(input$abo)
+      
+      price <- get_abo_price(con, input$course, input$abo)$abo_price
+      
+      updateNumericInput(
+        session,
+        "new_price",
+        value = price
+      )
+    })
+    
+    # UPDATE
+    observeEvent(input$update_price, {
+      req(input$course, input$abo, input$new_price)
+      
+      update_abo_price(con, input$course, input$abo, input$new_price)
     })
   })
 }
