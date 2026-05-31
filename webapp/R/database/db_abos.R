2508471d78da521010208b8c83b87e406011b3b5
Janik Schönbeck <59802740+333Jay333@users.noreply.github.com>
2026-05-31 17:20:22 +0200

    added checks before inserting attendance

--- webapp/R/database/db_abos.R
+++ webapp/R/database/db_abos.R
@@ -104,3 +104,35 @@ get_attended_courses_abo_10 <- function(con) {
   )
 }
 
+# query to get how many courses were attended already for members with 10er abo
+# only selects courses if the match the abo_id of the currently active abo
+get_attended_courses_abo_10_user_id <- function(con, user_id) {
+  dbGetQuery(
+    con,
+    "
+    SELECT 
+      m.user_id,
+      m.vorname,
+      m.name,
+      ab.abo_id,
+      COUNT(a.course_date_id) AS n_attended,
+      (10 - COUNT(a.course_date_id)) AS still_left
+    FROM members m
+    JOIN abos ab 
+      ON m.user_id = ab.user_id
+    LEFT JOIN attendance a 
+      ON m.user_id = a.user_id
+    WHERE ab.abo_type = 10
+      AND ab.abo_status = 'active'
+      AND a.abo_id = ab.abo_id
+      AND m.user_id = ?
+    GROUP BY m.user_id, m.vorname, m.name
+    ",
+    params = list(
+      user_id
+    )
+  )
+}
+
+
+
